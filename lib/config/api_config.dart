22d030608f12d7017a34775e4fc2bc5e950e1bbf
Khethani Savhasa <122065132+Elton2223@users.noreply.github.com>
2025-08-26 22:20:24 +0200

    URL change

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
   // Base URL for the LoopBack4 API
-  static const String baseUrl = 'http://10.50.93.130:3005';
+  static const String baseUrl = 'http://192.168.187.231:3005';
   
   // API Endpoints
   static const String userManagementEndpoint = '/user-management';
